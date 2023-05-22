949a957442d4c0629ed6e78044ec1800c1261e66
kuzmichev-stepa <kuzmichev.stepa@gmail.com>
2023-05-22 21:01:45 +0200

    Закоммичена строчка с time.sleep необходимая для проверки

--- test_items/test_items.py
+++ test_items/test_items.py
@@ -1,39 +1,21 @@
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import pytest
 import time
-import math
  
- link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_to_basket(browser):
     browser.implicitly_wait(10)
     browser.get(link)
-
-    # Открываем поп-ап авторизации
-    button = browser.find_element(By.ID, "ember33")
-    button.click()
     
-    #Находим элемент для ассерта
-    modal_dialog = browser.find_element(By.ID, "ember84")
-    
-    # Код, который заполняет обязательные поля
-    input1 = browser.find_element(By.ID, "id_login_email")
-    input1.send_keys("kuzmichev.stepa@yandex.ru")
-    input2 = browser.find_element(By.ID, "id_login_password")
-    input2.send_keys("W)8fLZg5LP2bV2.")
+    #time.sleep(30)
 
-    # Отправляем заполненную форму
-    button = browser.find_element(By.CSS_SELECTOR, "button.sign-form__btn")
-    button.click()
-
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(5)
-
-    # с помощью assert проверяем, что поп-ап закрылся
-    assert EC.visibility_of_element_located(modal_dialog) != True, "smth broken"
+    # Ищем кнопку добавления в корзину
+    button = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
+  
+    # с помощью assert проверяем, что кнопка добавленияв корзиину кликабельна
+    assert len(button) >0, "===Page haven't Button for add to basket==="
             
 if __name__ == "__main__":
-    pytest.main()
+   pytest.main()
